f9f3f1ac0bd12c052ea0846bb71cc503df72fab4
Lucas <lucas.quaresma1998@gmail.com>
2019-08-25 21:10:04 -0300

    resolvi o comflito de olas

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
 
-<<<<<<< HEAD
 	echo "Olá mundo 456!";
 
-	echo "Olá Udemy!";
-=======
-	echo "Olá mundo 123!";
->>>>>>> 0d09b6672ed00ddd41e88b543385ab3fc62e46fa
+	echo "Olá Udemy e Hcode!";
 
-	echo "Olá Hcode!";
 ?>
